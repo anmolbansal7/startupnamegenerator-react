e0326b3fecbe767c7cc72013767eba65fd47e6c4
anmolbansal7 <abanmolbansal5@gmail.com>
2022-01-04 16:27:23 +0530

    6/9

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="head-main">
+            <h1 className="head-text">Name It</h1>
+        </div>
+    );
+};
+
+export default Header;
